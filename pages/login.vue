dd4252161f914465181bc54c39f0c91646e9ec10
Justin High <novafluxx@users.noreply.github.com>
2025-05-01 20:12:56 -0400

    Edits to login page to center form fields and add autocompletes

--- pages/login.vue
+++ pages/login.vue
@@ -11,13 +11,13 @@
         </template>
 
         <!-- UForm for structure and potential validation -->
-        <UForm :state="{ email, password }" class="space-y-6" @submit="login">
+        <UForm :state="{ email, password }" class="space-y-6 mx-auto max-w-sm w-full flex flex-col items-center" @submit="login">
           <UFormField label="Email Address" name="email" required>
-            <UInput v-model="email" type="email" placeholder="you@example.com" icon="i-heroicons-envelope" />
+            <UInput v-model="email" type="email" placeholder="you@example.com" icon="i-heroicons-envelope" autocomplete="email" />
           </UFormField>
 
           <UFormField label="Password" name="password" required>
-            <UInput v-model="password" type="password" placeholder="Password" icon="i-heroicons-lock-closed" />
+            <UInput v-model="password" type="password" placeholder="Password" icon="i-heroicons-lock-closed" autocomplete="current-password" />
           </UFormField>
 
           <!-- Display error message using UAlert -->
