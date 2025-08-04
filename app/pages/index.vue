2d19c2ecf9ae938074d9c25eafcab9a009fa6069
Justin High <novafluxx@users.noreply.github.com>
2025-08-04 17:58:31 -0400

    Added link to changelog on index page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -10,6 +10,10 @@
       <p class="text-sm text-gray-500 dark:text-gray-400">
         or <NuxtLink to="/signup" class="font-medium text-primary-600 hover:text-primary-500 dark:text-primary-400 dark:hover:text-primary-300">Sign Up</NuxtLink>
       </p>
+      <p class="text-sm text-gray-500 dark:text-gray-400">
+        Be sure to check out the 
+        <NuxtLink to="/changelog" label="Changelog" class="font-medium text-primary-600 hover:text-primary-500 dark:text-primary-400 dark:hover:text-primary-300">Changelog</NuxtLink>
+      </p>
     </main>
   </div>
 </template>
