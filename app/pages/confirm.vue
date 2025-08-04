706c7a67b665237b6b3cbdd8f28195e9001cb40b
Justin High <novafluxx@users.noreply.github.com>
2025-08-04 17:51:09 -0400

    Remove redundent appheader from pages. AppHeader is included in app.vue

--- app/pages/confirm.vue
+++ app/pages/confirm.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <AppHeader />
+    
     <main class="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <UCard class="max-w-md w-full space-y-8">
         <!-- Conditional Header -->
