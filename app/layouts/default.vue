e96345e4ab383e805678f46f55fbf2f127f58285
Justin High <novafluxx@users.noreply.github.com>
2025-08-09 01:44:51 -0400

    fix: fix for scrolling issue on changelog page

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex flex-col h-screen">
+  <div class="flex flex-col min-h-dvh">
     <AppHeader />
-    <main class="flex-1 overflow-hidden">
+    <main class="flex-1 overflow-auto overscroll-contain">
       <slot />
     </main>
   </div>
